17130c7e20cd108d2eef81720967c567086f6cd5
Xyarvius <xonaxis.studio@googlemail.com>
2023-10-17 00:45:26 +0200

    small changes

--- scripts/greyOut.js
+++ scripts/greyOut.js
@@ -1,4 +1,13 @@
 function reply_click(clicked_id)
 {
     $('#' + clicked_id).addClass('greyOut');
+    localStorage.setItem(clicked_id, 'true');
+}
+
+function load_grey(clicked_id)
+{
+    var isGrey = localStorage.getItem(clicked_id)
+    if (isGrey) {
+        $('#' + clicked_id).addClass('greyOut');
+    }
 }
